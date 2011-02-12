adb523f8e7bb6453e85065f4c465ab3845080791
Hongli Lai (Phusion) <hongli@phusion.nl>
2011-02-12 09:45:52 +0100

    Should check for Bundler >= 1.0.10 because older versions are incompatible with RubyGems 1.5.

--- lib/platform_info/depcheck/bundler.rb
+++ lib/platform_info/depcheck/bundler.rb
@@ -1,14 +1,14 @@
 require 'platform_info/depcheck'
 
-PlatformInfo::Depcheck.define('bundler >= 1.0.0') do
-  name "Bundler >= 1.0.0"
+PlatformInfo::Depcheck.define('bundler >= 1.0.10') do
+  name "Bundler >= 1.0.10"
   website "http://gembundler.com/"
   define_checker do
     found, message = check_for_ruby_library('bundler')
     if found
       begin
         require 'bundler/version'
-        if Bundler::VERSION >= '1.0.0'
+        if Bundler::VERSION >= '1.0.10'
           [true]
         else
           [false, "only found older version #{Bundler::VERSION}"]
